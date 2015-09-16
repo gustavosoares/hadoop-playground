a8416103d3a369084de9c17f182d78b94579563d
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-16 08:23:22 -0300

    toptitle example

--- hadoop-playground/src/main/java/com/hadoop/playground/TopTitles.java
+++ hadoop-playground/src/main/java/com/hadoop/playground/TopTitles.java
@@ -1,7 +1,5 @@
 package com.hadoop.playground;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -156,6 +154,7 @@ public class TopTitles extends Configured implements Tool {
     public static class TopTitlesMap extends Mapper<Text, Text, NullWritable, TextArrayWritable> {
         Integer N;
         // TODO
+        private TreeSet<Pair<Integer, String>> countTopTitleMap = new TreeSet<Pair<Integer, String>>();
 
         @Override
         protected void setup(Context context) throws IOException,InterruptedException {
@@ -166,17 +165,31 @@ public class TopTitles extends Configured implements Tool {
         @Override
         public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
             // TODO
+            Integer count = Integer.parseInt(value.toString());
+            String word = key.toString();
+
+            countTopTitleMap.add(new Pair<Integer, String>(count, word));
+
+            if (countTopTitleMap.size() > this.N) {
+                countTopTitleMap.remove(countTopTitleMap.first());
+            }
         }
 
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
             // TODO
+            for (Pair<Integer, String> item : countTopTitleMap) {
+                String[] strings = {item.second, item.first.toString()};
+                TextArrayWritable val = new TextArrayWritable(strings);
+                context.write(NullWritable.get(), val);
+            }
         }
     }
 
     public static class TopTitlesReduce extends Reducer<NullWritable, TextArrayWritable, Text, IntWritable> {
         Integer N;
         // TODO
+        private TreeSet<Pair<Integer, String>> countTopTitleMap = new TreeSet<Pair<Integer, String>>();
 
         @Override
         protected void setup(Context context) throws IOException,InterruptedException {
@@ -187,62 +200,80 @@ public class TopTitles extends Configured implements Tool {
         @Override
         public void reduce(NullWritable key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException {
             // TODO
+            for (TextArrayWritable val: values) {
+                Text[] pair= (Text[]) val.toArray();
+
+                String word = pair[0].toString();
+                Integer count = Integer.parseInt(pair[1].toString());
+
+                countTopTitleMap.add(new Pair<Integer, String>(count, word));
+
+                if (countTopTitleMap.size() > this.N) {
+                    countTopTitleMap.remove(countTopTitleMap.first());
+                }
+            }
+
+            for (Pair<Integer, String> item: countTopTitleMap) {
+                Text word = new Text(item.second);
+                IntWritable value = new IntWritable(item.first);
+                context.write(word, value);
+            }
         }
     }
 
 }
 
 // >>> Don't Change
-//class Pair<A extends Comparable<? super A>,
-//        B extends Comparable<? super B>>
-//        implements Comparable<Pair<A, B>> {
-//
-//    public final A first;
-//    public final B second;
-//
-//    public Pair(A first, B second) {
-//        this.first = first;
-//        this.second = second;
-//    }
-//
-//    public static <A extends Comparable<? super A>,
-//            B extends Comparable<? super B>>
-//    Pair<A, B> of(A first, B second) {
-//        return new Pair<A, B>(first, second);
-//    }
-//
-//    @Override
-//    public int compareTo(Pair<A, B> o) {
-//        int cmp = o == null ? 1 : (this.first).compareTo(o.first);
-//        return cmp == 0 ? (this.second).compareTo(o.second) : cmp;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return 31 * hashcode(first) + hashcode(second);
-//    }
-//
-//    private static int hashcode(Object o) {
-//        return o == null ? 0 : o.hashCode();
-//    }
-//
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (!(obj instanceof Pair))
-//            return false;
-//        if (this == obj)
-//            return true;
-//        return equal(first, ((Pair<?, ?>) obj).first)
-//                && equal(second, ((Pair<?, ?>) obj).second);
-//    }
-//
-//    private boolean equal(Object o1, Object o2) {
-//        return o1 == o2 || (o1 != null && o1.equals(o2));
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "(" + first + ", " + second + ')';
-//    }
-//}
+class Pair<A extends Comparable<? super A>,
+        B extends Comparable<? super B>>
+        implements Comparable<Pair<A, B>> {
+
+    public final A first;
+    public final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <A extends Comparable<? super A>,
+            B extends Comparable<? super B>>
+    Pair<A, B> of(A first, B second) {
+        return new Pair<A, B>(first, second);
+    }
+
+    @Override
+    public int compareTo(Pair<A, B> o) {
+        int cmp = o == null ? 1 : (this.first).compareTo(o.first);
+        return cmp == 0 ? (this.second).compareTo(o.second) : cmp;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * hashcode(first) + hashcode(second);
+    }
+
+    private static int hashcode(Object o) {
+        return o == null ? 0 : o.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Pair))
+            return false;
+        if (this == obj)
+            return true;
+        return equal(first, ((Pair<?, ?>) obj).first)
+                && equal(second, ((Pair<?, ?>) obj).second);
+    }
+
+    private boolean equal(Object o1, Object o2) {
+        return o1 == o2 || (o1 != null && o1.equals(o2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ')';
+    }
+}
 // <<< Don't Change
