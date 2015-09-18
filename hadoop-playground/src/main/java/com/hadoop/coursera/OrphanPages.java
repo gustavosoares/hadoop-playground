d1596e89402527e34f466c72a75f4bb40d85a22a
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-17 21:31:54 -0300

    orphan pages

--- hadoop-playground/src/main/java/com/hadoop/coursera/OrphanPages.java
+++ hadoop-playground/src/main/java/com/hadoop/coursera/OrphanPages.java
@@ -29,13 +29,37 @@ public class OrphanPages extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         //TODO
-        return 0;
+        Job job = new Job(getConf(), "Orphan Pages");
+        job.setJarByClass(getClass());
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(LinkCountMap.class);
+        job.setReducerClass(OrphanPageReduce.class);
+
+
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             //TODO
+            // Line example: 2: 3 747213 1664968 1691047 4095634 5535664
+            String line = value.toString();
+            int pos_colon = line.indexOf(':');
+            String page = line.substring(0,pos_colon);
+            String links_to = line.substring(pos_colon + 1,line.length()).trim();
+            StringTokenizer tokenizer = new StringTokenizer(links_to);
+            while (tokenizer.hasMoreTokens()) {
+                String page_to = tokenizer.nextToken();
+                context.write(new IntWritable(Integer.valueOf(page_to)), new IntWritable(1));
+            }
+            context.write(new IntWritable(Integer.valueOf(page)), new IntWritable(1));
         }
     }
 
@@ -43,6 +67,11 @@ public class OrphanPages extends Configured implements Tool {
         @Override
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             //TODO
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            if (sum == 1) context.write(key, null);
         }
     }
 }
